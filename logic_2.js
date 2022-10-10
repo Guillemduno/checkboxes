dfc6db599f4e649eedbf8a1c3556c0ef6f06798c
Guillem Duñó <guillem.duno@ticxcat.cat>
2022-10-10 11:37:48 +0200

    enalble only certain number of checkboxes

--- logic_2.js
+++ logic_2.js
@@ -1,108 +1,38 @@
-var materies_visitant = 2;
-console.log('here');
+
+const maxim_materies = 2;
+
 $(document).ready(function(){
     
-    if ($("input[name=admin]").size() < 1) {
-        check();
-        $(document).on('click', '[name^=agregats]', check);
-    }
+    $(document).on('click', '[name^=testCheckboxes]', maxim_materies_visitant);
+
 });
 
-function check() {
-    maxim_materies_visitant();
-    bloqueig_s1s2();
-    bloqueig_n1n2();
-
-    $('[name^=agregats][class^=disabled]').attr('disabled', true);
-    $('[name^=agregats]:not([class^=disabled])').removeAttr('disabled');
-}
 
 function maxim_materies_visitant() {
-    var s0 = $('.s0').find('[name^=agregats]:checked').size();
-    var s1 = $('.s1').find('[name^=agregats]:checked').size();
-    var s2 = $('.s2').find('[name^=agregats]:checked').size();
+    //console.log('entra maxim materies');
 
-    if (s0+s1+s2 >= materies_visitant) {
-        $('[name^=agregats]').not(':checked').addClass('disabled_maxim');
+    // Selecció tots checbox checked
+    let qtty_mat_selec = document.querySelectorAll('input[type="checkbox"]:checked');
+
+    // Selecció tots checbox not checked
+    let qtty_mat_not_selec = document.querySelectorAll('input[type="checkbox"]:not(:checked)');
+
+    //console.log('materies seleccionades: '+qtty_mat_selec.length);
+    //console.log('materies no seleccionades: '+qtty_mat_not_selec.length);
+
+    if ( qtty_mat_selec.length >= maxim_materies) {
+        console.log('No pots seleccionar més matèries....');
+
+        for (let i = 0; i < qtty_mat_not_selec.length; ++i) {
+            qtty_mat_not_selec[i].setAttribute('disabled', true);
+            console.log(i+"bucle");
+        }
     } else {
-        $('[name^=agregats]').not(':checked').removeClass('disabled_maxim');
+        for (let i = 0; i < qtty_mat_not_selec.length; ++i) {
+            qtty_mat_not_selec[i].removeAttribute('disabled', true);
+            console.log(i+"bucle");
+        }
     }
-
-    /*if (s0 >= materies_visitant || s0 + ((s1 + s2)/2)  >= materies_visitant) {
-        $('[name^=agregats]').not(':checked').addClass('disabled_s0');
-    } else {
-        $('[name^=agregats]').not(':checked').removeClass('disabled_s0');
-    }
-
-    if (s1 >= materies_visitant) {
-        $('.s0').find('[name^=agregats]').not(':checked').addClass('disabled_s1');
-    } else {
-        $('.s0').find('[name^=agregats]').not(':checked').removeClass('disabled_s1');
-    }
-
-    if (s2 >= materies_visitant) {
-        $('.s0').find('[name^=agregats]').not(':checked').addClass('disabled_s2');
-    } else {
-        $('.s0').find('[name^=agregats]').not(':checked').removeClass('disabled_s2');
-    }
-
-    if (s0 + s1 >= materies_visitant) {
-        $('.s1,.s0').find('[name^=agregats]').not(':checked').addClass('disabled_s0s1');
-    } else {
-        $('.s1,.s0').find('[name^=agregats]').not(':checked').removeClass('disabled_s0s1');
-    }
-
-    if (s0 + s2 >= materies_visitant) {
-        $('.s2,.s0').find('[name^=agregats]').not(':checked').addClass('disabled_s0s2');
-    } else {
-        $('.s2,.s0').find('[name^=agregats]').not(':checked').removeClass('disabled_s0s2');
-    }*/
 }
 
-function bloqueig_s1s2() {
-     $('.s1,.s2').find('[name^=agregats]').each(function(){
 
-        if ($(this).closest('td').hasClass('s1')) {
-            if ($(this).is(':checked')) {
-                $(this).closest('tr').find('.s2').find('[name^=agregats]').addClass('disabled_s1s2_' + $(this).value);
-            } else {
-                $(this).closest('tr').find('.s2').find('[name^=agregats]').removeClass('disabled_s1s2_' + $(this).value);
-            }
-        } else if($(this).closest('td').hasClass('s2')) {
-            if ($(this).is(':checked')) {
-                $(this).closest('tr').find('.s1').find('[name^=agregats]').addClass('disabled_s1s2_' + $(this).value);
-            } else {
-                $(this).closest('tr').find('.s1').find('[name^=agregats]').removeClass('disabled_s1s2_' + $(this).value);
-            }
-        }
-    });
-}
-
-function bloqueig_n1n2() {
-    $('.s0,.s1,.s2').find('[name^=agregats]').each(function(){
-        var materia = $(this).val();
-        var materia2 = null;
-        var materia_cercada = null;
-        if (materia.substring(2,3) == '1') {
-            materia2 = materia.substring(0,2) + '2' + materia.substring(3);
-            materia_cercada = $('.mod').find('.s0,.s1,.s2').find('[name^=agregats][value='+materia2+']').val(); 
-            if (typeof materia_cercada === 'undefined') {
-                materia2 = materia.substring(0,2) + '2' + materia.substring(3,4) + '0' + materia.substring(5) ;
-            }
-        }else if (materia.substring(2,3) == '2') {
-            materia2 = materia.substring(0,2) + '1' + materia.substring(3);
-            materia_cercada = $('.mod').find('.s0,.s1,.s2').find('[name^=agregats][value='+materia2+']').val();
-            if (typeof materia_cercada === 'undefined') {
-                materia2 = materia.substring(0,2) + '1' + materia.substring(3,4) + '1' + materia.substring(5) ;
-            }
-        }else {
-            return;
-        }
-
-        if ($(this).is(':checked')) {
-            $('.mod').find('.s0,.s1,.s2').find('[name^=agregats][value='+materia2+']').addClass('disabled_n1n2_' + materia2);
-        } else {
-            $('.mod').find('.s0,.s1,.s2').find('[name^=agregats][value='+materia2+']').removeClass('disabled_n1n2_' + materia2);
-        }
-    });
-}
