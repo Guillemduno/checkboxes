02aa2b57e8e471ece849470149f9b7bffce981c5
Guillem Duñó <guillem.duno@ticxcat.cat>
2022-10-10 15:28:34 +0200

    prettier

--- logic.js
+++ logic.js
@@ -1,26 +1,22 @@
-
-
-
 function maxim_materies_visitant(container, numero_materies_seleccionables) {
+  const maxim_materies = numero_materies_seleccionables;
+  const myContainer = document.getElementById(container);
 
-    const maxim_materies    = numero_materies_seleccionables;
-    const myContainer       = document.getElementById(container);
+  // Selecció tots checbox checked
+  let qtty_mat_selec = myContainer.querySelectorAll('input[type="checkbox"]:checked');
 
-    // Selecció tots checbox checked
-    let qtty_mat_selec = myContainer.querySelectorAll('input[type="checkbox"]:checked');
+  // Selecció tots checbox not checked
+  let qtty_mat_not_selec = myContainer.querySelectorAll('input[type="checkbox"]:not(:checked)');
 
-    // Selecció tots checbox not checked
-    let qtty_mat_not_selec = myContainer.querySelectorAll('input[type="checkbox"]:not(:checked)');
+  if (qtty_mat_selec.length >= maxim_materies) {
+    //console.log('No pots seleccionar més matèries....');
 
-    if ( qtty_mat_selec.length >= maxim_materies) {
-        //console.log('No pots seleccionar més matèries....');
-
-        for (let i = 0; i < qtty_mat_not_selec.length; ++i) {
-            qtty_mat_not_selec[i].setAttribute('disabled', true);
-        }
-    } else {
-        for (let i = 0; i < qtty_mat_not_selec.length; ++i) {
-            qtty_mat_not_selec[i].removeAttribute('disabled', true);
-        }
+    for (let i = 0; i < qtty_mat_not_selec.length; ++i) {
+      qtty_mat_not_selec[i].setAttribute("disabled", true);
     }
+  } else {
+    for (let i = 0; i < qtty_mat_not_selec.length; ++i) {
+      qtty_mat_not_selec[i].removeAttribute("disabled", true);
+    }
+  }
 }
